50bc4614ea4e942292916ccacc0a9fbc39b1f149
sixteentoy <sixteentoy@gmail.com>
2020-11-29 18:09:26 +0000

    DB endpoint change

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    'mongodb://' + (process.env.IP || '172.17.0.1') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/mernproject'
 }
