6b6d47d7ff37d457c69c9f8421f4252715d1e440
sixteentoy <sixteentoy@gmail.com>
2020-11-29 18:05:46 +0000

    Some changes

--- client/core/Home.js
+++ client/core/Home.js
@@ -66,7 +66,7 @@ export default function Home({history}){
                 <Typography variant="body2" component="p" className={classes.credit} color="textSecondary">Photo by <a href="https://unsplash.com/@boudewijn_huysmans" target="_blank" rel="noopener noreferrer">Boudewijn Huysmans</a> on Unsplash</Typography>
                 <CardContent>
                   <Typography type="body1" component="p">
-                    Welcome to the MERN Social home page. 
+                    Welcome to the Levelup Social home page. 
                   </Typography>
                 </CardContent>
               </Card>
--- client/core/Menu.js
+++ client/core/Menu.js
@@ -18,7 +18,7 @@ const Menu = withRouter(({history}) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h6" color="inherit">
-        MERN Social
+        Levelup Social
       </Typography>
       <Link to="/">
         <IconButton aria-label="Home" style={isActive(history, "/")}>
