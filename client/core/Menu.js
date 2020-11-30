87d2d2af3bad15b21da865f3ba867326d292b203
sixteentoy <sixteentoy@gmail.com>
2020-11-30 14:10:32 +0000

    Chahge for CD

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -18,7 +18,7 @@ const Menu = withRouter(({history}) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h6" color="inherit">
-        Levelup Social!!!
+        Levelup Social
       </Typography>
       <Link to="/">
         <IconButton aria-label="Home" style={isActive(history, "/")}>
